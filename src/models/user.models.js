b089187429a1721dd337a5157d379de5b1434380
RamSrivastava01 <ramsrivastava20@gmail.com>
2025-07-25 16:47:15 +0530

    addes jwt token for authentication

--- src/models/user.models.js
+++ src/models/user.models.js
@@ -1,5 +1,6 @@
 import mongoose, { Schema, SchemaType } from "mongoose";
 import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
 const userSchema = new Schema(
     {
         username: {
@@ -53,4 +54,36 @@ userSchema.pre("save", async function (next) {
     this.password = bcrypt.hash(this.password, 10);
     next();
 });
+
+userSchema.methods.isPasswordCorrect = async function (password) {
+    return await bcrypt.compare(password, this.password);
+};
+
+userSchema.methods.generateAccessToken = async function () {
+    // short lives access token
+    return jwt.sign(
+        {
+            id: this._id,
+            email: this.email,
+            username: this.username,
+            fullname: this.fullname,
+        },
+        process.env.ACCESS_TOKEN_SECRET,
+        {
+            expiresIn: process.env.ACCESS_TOKEN_EXPIRY,
+        }
+    );
+};
+userSchema.methods.generateRefreshToken = async function () {
+    // short lives access token
+    return jwt.sign(
+        {
+            id: this._id,
+        },
+        process.env.REFRESH_TOKEN_SECRET,
+        {
+            expiresIn: process.env.REFRESH_TOKEN_EXPIRY,
+        }
+    );
+};
 export const User = mongoose.model("User", userSchema);
